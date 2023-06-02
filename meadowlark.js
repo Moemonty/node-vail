94c262ceff15c3da108b8320b6bd0c22d3c0614d
Moe Monty <moe@moe.com>
2023-06-01 20:36:48 -0500

    Code cleanup

--- meadowlark.js
+++ meadowlark.js
@@ -2,47 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 const { engine } = require ('express-handlebars');
+const handlers = require('./lib/handlers')
 
 app.engine('handlebars', engine({
     defaultLayout: 'main',
 }));
 
 app.set('view engine', 'handlebars');
-// app.set("views", "./views");
 
 // Express -- Order in which routes and middleware are added is significant
-
-app.get('/', (req, res) => res.render('home'))
-
-// app.get('/', (req, res) => {
-//     res.render('home');
-// });
-
-// app.get('/', (req, res) => {
-//     res.type('text/plain')
-//     res.send('Meadowlark Travel')
-// })
-
-// app.get('/about', (req, res) => {
-//     res.type('text/plain')
-//     res.send('About Meadowlark Travel')
-// })
-
-// Handlebars version
-app.get('/about', (req, res) => res.render('about'))
-
-app.use((req, res) => {
-    res.type('text/plain')
-    res.status(404)
-    res.send('404 - Not Found')
-})
-
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.type('text/plain')
-    res.status(500)
-    res.send('500 - Server Error')
-})
+app.get('/', handlers.home)
+app.get('/about', handlers.about)
+app.use(handlers.notFound);
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port}` + ` press Ctrl-C to terminate`
