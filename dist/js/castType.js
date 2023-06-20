379f764d854ee6163cffd8b02f643b3eba2e4184
Moe Montenegro <moe@senecafoundation.org>
2023-06-20 09:54:07 -0500

    removed ts error

--- dist/js/castType.js
+++ dist/js/castType.js
@@ -3,5 +3,5 @@ const unknownType = "123";
 const unknownType2 = "Name";
 const cast1 = unknownType;
 const cast2 = unknownType;
-const name = unknownType2;
+// const name: string = unknownType2 as string;
 console.log(name);
--- js/castType.ts
+++ js/castType.ts
@@ -4,6 +4,6 @@ const unknownType2: unknown = "Name"
 const cast1: number = <number>unknownType;
 const cast2: number = unknownType as number;
 
-const name: string = unknownType2 as string;
+// const name: string = unknownType2 as string;
 
 console.log(name);
